e71a173933d3ab08b14043df5a50896a4efe1153
nonnax <ironald@gmail.com>
2024-01-04 16:36:40 +0800

    update

--- vector_snake/main.lua
+++ vector_snake/main.lua
@@ -3,7 +3,7 @@ vector = require 'vector'
 sound = require 'sound'
 
 function love.load()
-	 h, w = love.graphics.getDimensions()
+	 w, h = love.graphics.getDimensions()
 
 	 sound:load()
 	 player = vector(100, 100)
@@ -22,10 +22,11 @@ function love.load()
 	 score = 0
 	 player_speed = 3
 end
+
 function love.update(dt)
 	 hit = false
 	 fruit_hit = false
-  -- local m = vector(love.mouse.getPosition())
+	 fruit_player_hit = false
 
 	 if vector.dist(player, pos) < player_radius*2 then
 	 		hit = true
@@ -48,7 +49,16 @@ function love.update(dt)
 			 		taillen = 0
 			 		table.remove(fruits, i)
 			 		sound.explode2:play()
-					 pos = -pos
+					 pos = vector(1, math.random(h))
+					 fruit_hit = true
+			 end
+			 if vector.dist(player, fruit) < fruit_radius*2 then
+			 		score = score - 1
+			 		table.remove(fruits, i)
+			 		sound.laser:play()
+					 dx = -dx
+					 dy = -dy
+					 fruit_player_hit = true
 			 end
 	 end
 	 table.insert(postail, pos)
@@ -78,9 +88,13 @@ function love.update(dt)
 	 	 dx = 1
 	 	 dy = 0
 	 end
+
 	 dir = vector(dx, dy)*player_speed
 	 -- dir:setmag(3)
 	 player = player + dir
+
+		check_player_edges()
+
 
 		if love.math.random(1, 100) == love.math.random(1, 100) then
 			  local fruit = vector(love.math.random(w), love.math.random(h))
@@ -100,29 +114,55 @@ end
 
 function love.keypressed(k)
 			if k == 'up' or k == 'down' or k == 'left' or k == 'right' then
-					player_speed = 3
+					player_speed = 3.5
 			end
 end
 
 function love.draw()
+	 love.graphics.setBackgroundColor({0, 0, 0})
+
  	love.graphics.circle('line', player.x, player.y, player_radius*2)
  	love.graphics.circle('fill', pos.x, pos.y, 5)
-
-	 if hit then
-	 	love.graphics.circle('line', player.x, player.y, math.random(1, 30))
-	 end
-
 
 	 for i, tail in ipairs(postail) do
 	 	 love.graphics.circle('line', tail.x, tail.y, 1)
 	 end
 
+	 if hit then
+	 	love.graphics.circle('line', player.x, player.y, math.random(1, 100))
+	 	love.graphics.circle('fill', pos.x, pos.y, math.random(1, 40))
+	 end
+
+
+	 if fruit_hit then
+	 	 for i=1, 10 do
+	 	 		love.graphics.setBackgroundColor({math.random(), math.random(), math.random()})
+	 	 end
+	 end
+
+
 	 for i, fruit in ipairs(fruits) do
-	 	 -- love.graphics.circle('line', fruit.x, fruit.y, 20)
-			 -- if fruit_hit then
 			 	love.graphics.circle('line', fruit.x, fruit.y, math.random(fruit_radius, fruit_radius*2))
-			 -- end
+			 	if fruit_player_hit then
+			 			love.graphics.circle('fill', player.x, player.y, math.random(100))
+			 	end
 	 end
 
 	 love.graphics.print(string.format("score: %d", score), 10, 10)
+end
+
+function check_player_edges()
+	 if player.x > w  then
+	 	 player.x = w
+	 	 dx = -dx
+	 elseif player.x < 0 then
+	 	 player.x = 0
+	 	 dx = -dx
+	 elseif player.y > h then
+	 	 player.y = h
+	 	 dy = -dy
+	 elseif player.y < 0 then
+	 	 player.y = 0
+	 	 dy = -dy
+	 end
 end
